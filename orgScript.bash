0972a54d7aab42ed9573bc67ffee14f5db63ade2
rosscwagner <rosscwagner@gmail.com>
2018-03-31 18:47:43 -0400

    Added a simple bash script that runs the orginizer every hour

---
+++ orgScript.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Starting to-do organizer"
+while true; do  
+	echo "compiling" 
+	gcc -o org toDoOrg.c
+	echo "running"
+	`./org toDo.txt`
+	
+	#sleep 
+	echo "Sleeping for 1 hour" 
+	sleep 3600 
+done
